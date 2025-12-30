5ba1cb68f6cc5728593a1c3482f0aa8c97cece1b
alphatee <alphatee.herrera@gmail.com>
2025-12-30 12:40:23 -0800

    added get_book_text function

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath:str) -> str:
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+
+
+main()
