b30976a7518ed0269d8d361a8d06d52e943634dc
alphatee <alphatee.herrera@gmail.com>
2025-12-30 13:19:36 -0800

    Added a new function to log frequency of characters

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from stats import get_book_text, get_word_count
+from stats import get_book_text, get_word_count, get_character_count
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
+
     num_words = get_word_count(book_text)
+    num_characters = get_character_count(book_text)
+
     print(f"Found {num_words} total words")
+    print(num_characters)
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def get_book_text(path_to_file:str) -> str:
     with open(path_to_file) as f:
         file_contents = f.read()
@@ -6,3 +8,7 @@ def get_book_text(path_to_file:str) -> str:
 def get_word_count(file_contents:str) -> int:
     word_count = file_contents.split()
     return len(word_count)
+
+def get_character_count(file_contents:str) -> dict[str,int]:
+    format_data = file_contents.lower()
+    return Counter(format_data)
