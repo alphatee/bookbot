3c5223724f77909b59b3424fe5321faa10ac789b
alphatee <alphatee.herrera@gmail.com>
2025-12-31 03:54:07 -0800

    Added Arguments functionality

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from stats import get_book_text, get_num_words, get_chars_dict, chars_dict_to_sorted_list
+import sys
 
 ####################################################################
 
 def main():
-    file_name = "books/frankenstein.txt"
+    ####################################################################
+    # Explain how to use program
+    ####################################################################
+    print("============ MAN PAGE ============")
+    print("Usage: python3 main.py <path_to_book>")
+    print(''' Path to Book listed below:
+    books/frankenstein.txt
+    books/mobydick.txt
+    books/prideandprejudice.txt
+    ''')
+    if sys.argv[0] and sys.argv[1] == False:
+        sys.exit(1)
+
+    ####################################################################
+    # Execute Program
+    ####################################################################
+    file_name = sys.argv[1]
     book_text = get_book_text(file_name)
 
     num_words = get_num_words(book_text)
@@ -28,6 +45,6 @@ def main():
     print("============= END ===============")
 
 ####################################################################
-                #Entry Point#
+# Entry Point
 ####################################################################
 main()
