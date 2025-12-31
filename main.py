97e9fe9e98b9170687bad5e879656b45f4700785
alphatee <alphatee.herrera@gmail.com>
2025-12-31 03:32:11 -0800

    Print a Report

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from stats import get_book_text, get_word_count, get_character_count
+from stats import get_book_text, get_num_words, get_chars_dict, chars_dict_to_sorted_list
+
+####################################################################
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    file_name = "books/frankenstein.txt"
+    book_text = get_book_text(file_name)
 
-    num_words = get_word_count(book_text)
-    num_characters = get_character_count(book_text)
+    num_words = get_num_words(book_text)
+    num_characters = get_chars_dict(book_text)
 
+    # print(f"Found {num_words} total words")
+    # print(num_characters)
+
+    ####################################################################
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_name}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(num_characters)
+    print("--------- Character Count -------")
 
+    ###
+    list_of_dicts = chars_dict_to_sorted_list(num_characters)
+    for item in list_of_dicts:
+        print(f"{item["char"]}: {item["num"]}")
+    ###
 
+    print("============= END ===============")
+
+####################################################################
+                #Entry Point#
+####################################################################
 main()
--- stats.py
+++ stats.py
@@ -5,10 +5,32 @@ def get_book_text(path_to_file:str) -> str:
         file_contents = f.read()
     return file_contents
 
-def get_word_count(file_contents:str) -> int:
+def get_num_words(file_contents:str) -> int:
     word_count = file_contents.split()
     return len(word_count)
 
-def get_character_count(file_contents:str) -> dict[str,int]:
+def get_chars_dict(file_contents:str) -> dict[str,int]:
     format_data = file_contents.lower()
     return Counter(format_data)
+
+def chars_dict_to_sorted_list(data:dict[str,int]) -> list[dict[str,int]]:
+    new_list = []
+
+    # filter input to exclude non alpha characters
+    for key, value in data.items():
+        if not key.isalpha():
+            continue
+
+        # format input as a composite dictionary of two dictionaries
+        new_dict = { "char": key, "num": value }
+        # add the composite dictionary to the list
+        new_list.append(new_dict)
+
+    # sort the new list from greatest to least frequency and sort on the composite dictionary key
+    new_list.sort(reverse=True, key=sort_on)
+    # return the new list of composite dictionaries
+    return new_list
+
+# helper free fuction
+def sort_on(data:dict[str,int]) -> int:
+    return data["num"]
